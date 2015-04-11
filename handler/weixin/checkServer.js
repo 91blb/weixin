39522e85cc2d2d6a22e886ccfd3168d4718644ff
ldjking <ldjbox@126.com>
2015-04-11 14:15:18 +0800

    如果是post请求

--- handler/weixin/checkServer.js
+++ handler/weixin/checkServer.js
@@ -1,23 +1,30 @@
 var check=require("../../lib/weixin/checkToken");
 module.exports=function(req,res,cb){//微信校验
-	var now=new Date();
-	var query=req.query;
-	
-	var signature=query.signature;
-	var timestamp=query.timestamp;
-	var nonce=query.nonce;
-	var echostr=query.echostr;
-	
-	console.log(now.getHours()+":"+now.getMinutes()+":"+now.getSeconds()+" "+now.getMilliseconds());
 
-	console.log("wexin check",req.query);
-	var result=check(signature,timestamp,nonce,echostr);
-	console.log("check result=["+result+"]");
-	if(result){
-		return echostr;//如果校验通过 按照微信要求 原样返回echostr
+	console.log("method",req.method);
+	if(req.method=="GET"){
+		var now=new Date();
+		var query=req.query;
+		
+		var signature=query.signature;
+		var timestamp=query.timestamp;
+		var nonce=query.nonce;
+		var echostr=query.echostr;
+		
+		console.log(now.getHours()+":"+now.getMinutes()+":"+now.getSeconds()+" "+now.getMilliseconds());
+
+		console.log("wexin check",req.query);
+		var result=check(signature,timestamp,nonce,echostr);
+		console.log("check result=["+result+"]");
+		if(result){
+			return echostr;//如果校验通过 按照微信要求 原样返回echostr
+		}
+		else{
+			return false;
+		}
 	}
 	else{
-		return false;
+		console.log("body",req.body);
 	}
 	//return result;
 }
