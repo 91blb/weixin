51d2373daa4d8e4872ccbc99bbddb4625baad57e
ldjking <ldjbox@126.com>
2015-07-06 23:17:21 +0800

    提交代码

--- handler/weixin/getBullet.js
+++ handler/weixin/getBullet.js
@@ -25,13 +25,13 @@ module.exports=function(req,res,opt){
 	console.log("body",req.body);
 	var mobile=req.body.mobile;
 	var uid=req.body.weixin_uid;
-	var shareUrl=getAuthUrl(mobile,uid);
+	//var shareUrl=getAuthUrl(mobile,uid);
 
 	var result={
 		code:0,//领取红包成功
-		amount:10,
+		amount:180,
 		url:"https://m.nongfadai.com/register.html?code="+mobile,
-		myBonusShareUrl:shareUrl//分享的地址 要带上一些额外的参数 后台辅助生成//分享的地址要重新带上用户的参数信息，比如
+		myBonusShareUrl:"#"//分享的地址 要带上一些额外的参数 后台辅助生成//分享的地址要重新带上用户的参数信息，比如
 	}
 	return result;
 }
