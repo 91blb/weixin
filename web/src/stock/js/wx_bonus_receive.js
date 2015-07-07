3a01380d824a7186149b291d3f08bd0f86dec29c
ldjking <ldjbox@126.com>
2015-07-07 17:27:05 +0800

    提交代码

--- web/src/stock/js/wx_bonus_receive.js
+++ web/src/stock/js/wx_bonus_receive.js
@@ -214,7 +214,7 @@ $(function(result) {
 		}
 		var title = "股市反击第一枪，农发贷邀你来一发";
 		var des = "敢承诺4500点以下不卖出？壮士请受农发贷一拜，180元红包请笑纳。";
-		var imgUrl = "http://www.51blb.com/stock/img/gun3_256.jpg";//
+		var imgUrl = "http://www.51blb.com/stock/img/gun3_256_2.jpg";//
 		var linkUrl = wxconf.shareUrl;//分享链接
 		wx.onMenuShareTimeline({
 			title: title + des, // 分享标题
