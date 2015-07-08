b51a2a4a824278692459f8860c6f6cb272c0a33b
ldjking <ldjbox@126.com>
2015-07-08 09:01:47 +0800

    提交本地代码

--- handler/weixin/shareSucc.js
+++ handler/weixin/shareSucc.js
@@ -1,56 +1,22 @@
 var redis=require("../../lib/redis/api/redis_promise");
 //console.log(redis);
 
-function getAuthUrl(phone,uid){
-	//生成一个要求weixin做oauth认证的url 
-	console.log("本利保");
-	var appid="wx4b6e962611f5e662";
-
-	//console.log("农发贷");
-	//var appid="wxdd2f29ae7d42c94d";
-
-	//var myUrl="http://www.51blb.com/handler/weixin/bonus.html";
-	var myUrl="http://www.51blb.com/handler/weixin/stock.html?phone="+phone+"&uid="+uid;
-	var baseUrl="https://open.weixin.qq.com/connect/oauth2/authorize?";
-	var url=baseUrl+"appid=";
-	url+=appid;
-	url+="&redirect_uri=";
-	url+=encodeURIComponent(myUrl);
-	//url+="&response_type=code&scope=snsapi_userinfo";//需要用户授权
-	url+="&response_type=code&scope=snsapi_base";//静默授权
-	url+="&state=STATE&connect_redirect=1#wechat_redirect";
-
-	console.log(url);
-	return url;
-}
 
 module.exports=function(req,res,opt){
-	console.log("body",req.body);
-	var mobile=req.body.mobile;
-	var wxuid=req.body.wxuid;
+	console.log("share succ body",req.body);
+	var wxuid=req.body.wxuid;	
 	var source=req.body.source;//来源
+	var type=req.body.type;//分享方式 朋友圈 朋友
 
-	var key="table:register:wxuid:"+wxuid;
-	var data={
-		wxuid:wxuid,
-		mobile:mobile,
-		source:source
-	}
-
-	redis.savejson(key,data)
+	var key_share="table:shareweixin:uuid:"+redis.nextSeq("shareweixin");/*read 点击过某人分享链接的人*/
+	redis.savejson(shareweixin,{wxuid:wxuid,readtime:new Date(),source:source})
 	.then(function(result){
-		console.log("save recommed relation result",result);
+		console.log("save share record result",result);
 	})
 	.catch(function(err){
-		console.log("save recommed relation err",err);
-	});
-	var shareUrl=getAuthUrl(mobile,wxuid);
-	var result={
-		code:0,//领取红包成功
-		amount:180,
-		url:"https://m.nongfadai.com/register.html?code="+mobile,
-		myBonusShareUrl:shareUrl//分享的地址 要带上一些额外的参数 后台辅助生成//分享的地址要重新带上用户的参数信息，比如
-	}
-	return result;
+		console.log("save share record err",err);
+	})
+
+	return 0;
 
 }
