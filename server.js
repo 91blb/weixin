6290a69abcc056940d8d24105c1ee2c3cedf31e4
ldjking <ldjbox@126.com>
2016-01-15 12:44:20 +0800

    提交代码

--- server.js
+++ server.js
@@ -143,13 +143,13 @@ function handler(req, res) { //处理所有服务请求
 
 
 app.use("/handler/", handler); //服务处理程序 handler
-//app.use("/", express.static(__dirname + "/web/src/")); //服务处理程序 handler
+app.use("/", express.static(__dirname + "/web/src/")); //服务处理程序 handler
 app.use("/src", express.static(__dirname + "/src/")); //服务处理程序 handler
 app.use("/web/", express.static(__dirname + "/web/")); //web
 app.use("/web/src/", express.static(__dirname + "/web/src/")); //静态资源web
 app.use("/test/", express.static(__dirname + "/test/")); //静态资源web
 
-app.use("/", express.static(__dirname + "/web/src/")); //服务处理程序 handler
+//app.use("/", express.static(__dirname + "/web/src/")); //服务处理程序 handler
 app.use("/financing/hqb/", express.static(__dirname + "/src/")); //服务处理程序 handler
 app.use("/static/", express.static("D:/code/dev/static/src/main/webapp/")); //服务处理程序 handler
 
